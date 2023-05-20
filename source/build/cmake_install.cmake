2d1bae4c4562397ab49628934db2bfa0abd09845
Matfay <mateo.fayet04@gmail.com>
2023-05-20 16:44:04 +0200

    mc.synGranul~ mxe64 create

--- source/build/cmake_install.cmake
+++ source/build/cmake_install.cmake
@@ -67,6 +67,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/bargraf~/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/mc.synGranul~/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
