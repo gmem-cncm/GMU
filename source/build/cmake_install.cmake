ec6b2f0cb56fb25a443d9d2207072d624d70e5b5
Matfay <mateo.fayet04@gmail.com>
2023-05-20 14:56:12 +0200

    windows compiling

--- source/build/cmake_install.cmake
+++ source/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/arthus/Documents/GitHub/GMU/source
+# Install script for directory: C:/Users/mateo/Documents/GMEM Project/GMU/source
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/source")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -32,44 +32,39 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/mc.bufGranul~/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/arthus/Documents/GitHub/GMU/source/build/mc.bufGranul~/cmake_install.cmake")
+  include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/liveGranul~/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/arthus/Documents/GitHub/GMU/source/build/liveGranul~/cmake_install.cmake")
+  include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/synGranul~/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/arthus/Documents/GitHub/GMU/source/build/synGranul~/cmake_install.cmake")
+  include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/rand_dist_list/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/arthus/Documents/GitHub/GMU/source/build/rand_dist_list/cmake_install.cmake")
+  include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/rand_dist_list~/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/arthus/Documents/GitHub/GMU/source/build/rand_dist_list~/cmake_install.cmake")
+  include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/idleubutton/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/arthus/Documents/GitHub/GMU/source/build/idleubutton/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/Users/arthus/Documents/GitHub/GMU/source/build/bargraf~/cmake_install.cmake")
+  include("C:/Users/mateo/Documents/GMEM Project/GMU/source/build/bargraf~/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -80,5 +75,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/arthus/Documents/GitHub/GMU/source/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/mateo/Documents/GMEM Project/GMU/source/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- source/build/rand_dist_list~/cmake_install.cmake
+++ source/build/rand_dist_list~/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/arthus/Documents/GitHub/GMU/source/rand_dist_list~
+# Install script for directory: C:/Users/mateo/Documents/GMEM Project/GMU/source/rand_dist_list~
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/source")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -30,10 +30,5 @@ endif()
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
